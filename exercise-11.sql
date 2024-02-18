2ee349366993c5f12e840346f57d56d34b4e2d2d
Bilge <bilgeates.ba@gmail.com>
2024-02-18 13:45:43 +0100

    UNION, INTERSECT and EXCEPT exercises

---
+++ exercise-11.sql
@@ -0,0 +1,26 @@
+--1. actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+(
+select first_name from actor
+)
+union
+(
+select first_name from customer
+)
+
+--2. actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+(
+select first_name from actor
+)
+intersect
+(
+select first_name from customer
+)
+
+--3. actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+(
+select first_name from actor
+)
+except
+(
+select first_name from customer
+)
